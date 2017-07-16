de921dfc8ef8880eb993c2b684b3902c47e04d9f
Michael Rubanov <mrubanov@gmail.com>
2017-07-16 04:31:59 +0300

    hexagon pod init routine

--- src/Cli.js
+++ src/Cli.js
@@ -1,5 +1,103 @@
+import cliparse from "cliparse";
+import fs from "fs";
+
+const PARSERS = cliparse.parsers;
+
 class Cli {
-  constructor() {
+  init(options) {
+    var dirname = this.handleOptions({destination: options.args[0]});
+    console.log(this);
+    this.next(null, options);
+  }
+  add(o) {
+    this.next(null, o);
+  }
+  build(o) {
+    this.next(null, o);
+  }
+  handleOptions(options) {
+    let self = this;
+    if (options instanceof Object) {
+      if (options.destination) {
+        let dirname = options.destination;
+        if (typeof dirname === "string") {
+          if (dirname.length) {
+            let error = fs.accessSync(
+              dirname,
+              fs.constants.R_OK | fs.constants.W_OK
+            );
+            if (error) {
+              throw new Error(error);
+            } else {
+              self.dirname = dirname;
+              let destDirContent = fs.readdirSync(dirname);
+              if (destDirContent.length) {
+                throw new Error(
+                  "Destination folder " + dirname + " is not empty."
+                );
+              } else {
+                return dirname;
+              }
+            }
+          } else {
+            throw new Error("Please define destination directory.");
+          }
+        } else {
+          throw new Error("Please define destination directory properly.");
+        }
+      }else{
+        throw new Error("Please define destination directory properly 2.");
+      }
+    }
+  }
+  initSettings(){
+    this.parser = cliparse.cli({
+      name: "hexagon-cli",
+      description: "hexagon command line interface.",
+      commands: [
+        cliparse.command(
+          "init",
+          {
+            description: "Initialize new hexagon pod in current directory.",
+            options: [],
+            args: [
+              cliparse.argument("destination", {
+                description: "directory in which pod will be initiated."
+              })
+            ]
+          },
+          this.init.bind(this)
+        ),
+        cliparse.command(
+          "build",
+          {
+            description: "Build current pod into single html page.",
+            args: [],
+            options: []
+          },
+          this.build.bind(this)
+        ),
+        cliparse.command(
+          "add",
+          {
+            description: "Add record to current hexagon pod.",
+            args: [
+              cliparse.argument("title", { description: "new record's title." })
+            ],
+            options: []
+          },
+          this.add.bind(this)
+        )
+      ]
+    });
+  }
+  constructor(next = () => {}) {
+    this.initSettings();
+    this.next = next;
+  }
+  parse(argv) {
+    return cliparse.parse(this.parser, argv);
   }
 }
 export default Cli;
+
--- test/CliSpec.js
+++ test/CliSpec.js
@@ -2,37 +2,45 @@ import { expect } from "chai";
 import Cli from "../src/Cli";
 
 describe("Cli", () => {
-  describe("Cli.constructor()", () => {
-    const cli = new Cli();
+  //let cli = new Cli();
 
-    it("should be and instance of `Object`.", () => {
-      expect(cli).to.be.instanceOf(Object);
-    });
-    //it('', () => {
-    //expect(false).to.be.true;
+  //describe("Cli.constructor()", () => {
+    //it("should be and instance of `Object`.", () => {
+      //expect(cli).to.be.instanceOf(Object);
     //});
-  });
+  //});
 
-  describe("Cli.initializePOD()", () => {
-    it("should be able to handle `hexagon pod` initialization.", () => {
-      expect(false).to.be.true;
-    });
-  });
-
-  describe("Cli.build()", () => {
-    it(
-      "should be able to handle build an Single html page,        \
-       containing all neccessary `hexagon pot` content to function.",
-      () => {
-        expect(false).to.be.true;
+  describe("cli.parse()", () => {
+    it("should return parsed as instance of Object.", () => {
+      let next = (err, result) => {
+        expect(result).to.be.instanceOf(Object)
       }
-    );
-  });
-
-  describe("Cli.add()", () => {
-    it("should be able to handle addition of new content to `hexagon`.", () => {
-      expect(false).to.be.true;
+      let cli = new Cli(next);
+      let argv = ['','init', '/home/michaelr/ttt']
+      let result = cli.parse(argv);
     });
   });
+
+  //describe("Cli.initializePOD()", () => {
+    //it("should be able to handle `hexagon pod` initialization.", () => {
+      //expect(false).to.be.true;
+    //});
+  //});
+
+  //describe("Cli.build()", () => {
+    //it(
+      //"should be able to handle build an Single html page,        \
+       //containing all neccessary `hexagon pot` content to function.",
+      //() => {
+        //expect(false).to.be.true;
+      //}
+    //);
+  //});
+
+  //describe("Cli.add()", () => {
+    //it("should be able to handle addition of new content to `hexagon`.", () => {
+      //expect(false).to.be.true;
+    //});
+  //});
 });
 
